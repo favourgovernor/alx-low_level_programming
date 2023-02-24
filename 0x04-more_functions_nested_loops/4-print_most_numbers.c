b64b8265ac8940f7969fadf067f9e56439b9a733
favourgovernor <favourirungu99@gmail.com>
2023-02-24 06:23:02 -0800

    4-main.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -20,14 +20,14 @@ void print_most_numbers(void)
 
 	for (number = 0; number <= 9; number++)
 	{
-		if (number == 2 && number == 4)
-		{
-			continue;
-		}
-		else
-		{
-			_putchar(number + '0');
-		}
+	if (number == 2 || number == 4)
+	{
+	continue;
+	}
+	else
+	{
+	_putchar(number + '0');
+	}
 	}
 	_putchar('\n');
 }
