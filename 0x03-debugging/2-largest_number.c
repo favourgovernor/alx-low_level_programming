45794e5fc62cfc8dc3ff0421c334af2bfe0da640
favourgovernor <favourirungu99@gmail.com>
2023-02-24 10:32:43 -0800

    largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -18,50 +18,20 @@ int largest_number(int num1, int num2, int num3);
  * Return: the largest number
  */
 
-int largest_number(int num1, int num2, int num3)
+int largest_number(int a , int b, int c)
 {
 	int largest;
 
-	if (num1 > num2 && num1 > num3)
+	if(a >= b && a >= c)
 	{
-		largest = num1;
+	largest = a;
 	}
-	else if (num2 > num1 && num2 > num3)
+	else if (b >= a && b >= c)
 	{
-		largest = num2;
-	}
-	else if (num3 > num1 && num3 > num2)
+	largest = b;
+        else
 	{
-		largest = num3;
+	largest = c;	
 	}
-	else if (num1 == num2 && num1 > num3)
-	{
-		largest = num1;
-	}
-	else if (num1 == num3 && num1 > num2)
-	{
-		largest = num1;
-	}
-	else if (num2 == num3 && num2 > num1)
-	{
-		largest = num2;
-	}
-	else if (num2 == num1 && num2 > num3)
-	{
-		largest = num2;
-	}
-	else if (num3 == num1 && num3 > num2)
-	{
-		largest = num3;
-	}
-	else if (num3 == num2 && num3 > num1)
-	{
-		largest = num3;
-	}
-	else
-	{
-		largest = num1 = num2 = num3;
-	}
-
-	return (largest);
+        return (largest);
 }
