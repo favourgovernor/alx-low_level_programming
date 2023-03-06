f0b99acbb9c0e1b12e8fdb9a6067099202884870
favourgovernor <favourirungu99@gmail.com>
2023-03-06 14:40:54 -0800

    7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_chessboard - function that prints the chessboard
+ * @a: character variable
+ * Return: Success
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int row;
+	int col;
+
+	for (row = 0; row < 8; row++)
+	{
+		for (col = 0; col < 8; col++)
+		{
+			printf("%c", a[row][col]);
+		}
+		printf("\n");
+	}
+}
